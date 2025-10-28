700acd5f204c51bb0d7d936791eda6613b5d649f
Max <maxsiepsiak238@gmail.com>
2025-10-28 20:31:40 +0100

    if statement

---
+++ src/h3/H3_main.java
@@ -0,0 +1,17 @@
+package h3;
+
+public class H3_main {
+    public static void main(String[] args) {
+        int i = 10, j = 14, k = -10;
+
+        if (i > j) {
+            if(i <= 200) {
+                
+            }
+
+            if(i <= 100){
+
+            }
+        }
+    }
+}
